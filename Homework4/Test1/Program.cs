08c23405266ee7c4ed7f533681ebea922186aa85
nkyab <2053692914@qq.com>
2021-03-25 21:17:40 +0800

    homework4

---
+++ Homework4/Test1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node()
+        {
+            Next = null;
+            Data = default(T);
+        }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    class List<T>
+    {
+        public Node<T> Head;
+        public Node<T> Tail;
+
+        public List()
+        {
+            Head = new Node<T>();
+            Tail = new Node<T>();
+            Head.Next = Tail;
+        }
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            node.Next = Head.Next;
+            Head.Next = node;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node_p = this.Head.Next;
+
+            while (node_p != Tail)
+            {
+                action(node_p.Data);
+                node_p = node_p.Next;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        public static void DoThis<T>(T t)
+        {
+            Console.Write(t);
+            Console.Write("   ");
+        }
+
+        delegate int Fun(int n);
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Action<int> action = DoThis;
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.ForEach(action);
+
+            Fun fun = delegate (int k) {
+                return k;
+            };
+
+            Node<int> p = list.Head;
+
+            int max = p.Data;
+            int min = p.Data;
+            int total = 0;
+            while (p != list.Tail)
+            {
+                if ((p.Data) >= max)
+                {
+                    max = (p.Data);
+                }
+                else
+                {
+                    min = (p.Data);
+                }
+                total += (p.Data);
+                p = p.Next;
+            }
+            Console.WriteLine();
+            Console.WriteLine("最大值:" + max);
+            Console.WriteLine("最小值:" + min);
+            Console.WriteLine("和:" + total);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework4/Test2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public delegate void Clock(object sender, ClockTime clockTime);
+    public class ClockTime
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public ClockTime() { }
+        public ClockTime(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+        public void SetClockTime(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+    }
+
+    public class ClockOn
+    {
+        public event Clock Tick;
+        public event Clock Alarm;
+
+        public ClockTime AlartClockTime;
+
+        public void ClockTick(ClockTime clockTime)
+        {
+            if (clockTime.Second < 59)
+            {
+                clockTime.Second++;
+            } else if (clockTime.Second >= 59)
+            {
+                clockTime.Second = 0;
+                clockTime.Minute++;
+            }else if (clockTime.Minute >= 59)
+            {
+                clockTime.Minute = 0;
+                clockTime.Hour++;
+            }else if (clockTime.Hour >= 23)
+            {
+                clockTime.Hour = 0;
+            }
+            Console.WriteLine($"时间：{clockTime.Hour}:{clockTime.Minute}:{clockTime.Second}");
+            Tick(this, clockTime);
+        }
+
+        public void ClockAlarm(ClockTime clockTime, int hour, int minute, int second)
+        {
+            if (clockTime.Second == second && clockTime.Minute == minute && clockTime.Hour == hour)
+            {
+                Console.WriteLine("滴滴滴！！！");
+            }
+            Alarm(this, clockTime);
+        }
+    }
+
+    class Form
+    {
+        public ClockOn Clockon = new ClockOn();
+        public Form()
+        {
+            Clockon.Tick += Clockon_Tick;
+            Clockon.Alarm += Clockon_Alarm;
+        }
+
+        private void Clockon_Alarm(object sender, ClockTime clockTime)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("闹钟未激活......");
+        }
+
+        private void Clockon_Tick(object sender, ClockTime clockTime)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("时钟在走！");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockTime clockTime = new ClockTime(0, 0, 0);
+            ClockOn clockOn = new ClockOn();
+            Form form1 = new Form();
+            
+            for(int i = 0; i <= 602; i++)
+            {
+                form1.Clockon.ClockTick(clockTime);
+                form1.Clockon.ClockAlarm(clockTime, 0, 3, 20);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
